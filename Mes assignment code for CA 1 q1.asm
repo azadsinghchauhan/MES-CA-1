f995859a083f21ff4ac8e8eb1a46c663420cc455
Azad Singh Chauhan <chauhanazad007@gmail.com>
2026-02-05 14:02:47 +0530

    Update and rename Mes assignment code for CA 1.asm to Mes assignment code for CA 1 q1.asm

--- Mes assignment code for CA 1 q1.asm
+++ Mes assignment code for CA 1 q1.asm
@@ -2,7 +2,7 @@ MOV A, #5        ; A = 5
 ADD A, #5        ; A = 10
 
 MOV B, #100      ; B = 100
-MUL AB           ; A = 10 × 100 = 1000
+MUL AB           ; A = 10 Ã— 100 = 1000
 
 MOV B, #38       ; B = 38
 ADD A, B         ; A = 1038
